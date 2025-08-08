f4f28e7f3a4b02fd8bb28470bc2983cf785e720c
Orxan Celilov <orxan.celilov18@gmail.com>
2025-08-09 02:40:13 +0400

    yeni

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -218,7 +218,7 @@ const toggleWishlist = () => {
           event.stopPropagation();
           toggleWishlist();
         }}
-      className="absolute top-3 right-3 bg-black/60 hover:bg-black/80 rounded-full p-2 z-10"
+      className="absolute top-3 right-3 rounded-full p-2 z-10"
     >
       {isInWishlist ? (
         <img src="/icons/check.png" alt="In Wishlist" className="w-5 h-5" />
